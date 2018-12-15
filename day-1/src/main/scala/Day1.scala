89afdaca451e1584b490a76df1a969d2561a3f0d
ccolin84 <ccolin84@gmail.com>
2018-12-14 20:08:14 -0500

    solve day 1 part 2

--- day-1/src/main/scala/Day1.scala
+++ day-1/src/main/scala/Day1.scala
@@ -2,11 +2,14 @@ import scala.io.Source
 
 object Day1 {
   def main(args: Array[String]): Unit = {
+    val inputFile = "src/input.txt"
+    // The answer to part 1
     val totalChangeFromFile = calculateTotalFromFile(inputFile)
+    // The answer to part 2
+    val firstRepeatedTotal = calculateFirstRepeatedTotalFromFile(inputFile)
     println(s"The total change from the input file is: $totalChangeFromFile")
+    println(s"The first repeated total is: $firstRepeatedTotal")
   }
-
-  val inputFile = "src/input.txt"
 
   def parseInputLine(line: String): Tuple2[Char, Int] = {
     val operator = line.charAt(0)
@@ -19,23 +22,56 @@ object Day1 {
     operator match {
       case '+' => total + number
       case '-' => total - number
+      case other => throw new IllegalArgumentException(s"expected + or - but found $other")
     }
   }
 
-  def calculateTotalFromFile(fileName: String) = {
+  def forEachTotalFromFile(fileName: String, handler: Int => Boolean): Unit = {
     val bufferedSource = Source.fromFile(fileName)
     try {
       var total = 0
-      for (line <- bufferedSource.getLines) {
+      var continue = true
+      bufferedSource.getLines.withFilter(? => continue).foreach(line => {
         total = updateTotalFromLine(total, line)
-      }
-      total
+        continue = handler(total)
+      })
     } catch {
       case e: Exception => {
         println("Something went wrong!")
       }
     } finally {
       bufferedSource.close
+    }
+  }
+
+  // calculates the answer for Day 1 part 1
+  def calculateTotalFromFile(fileName: String): Int = {
+    var total = 0
+    forEachTotalFromFile(fileName, (newTotal: Int) => {
+      total = newTotal
+      true
+    })
+    total
+  }
+
+  // calculates the answer for Day 1 part 2
+  def calculateFirstRepeatedTotalFromFile(fileName: String, startingTotal: Int = 0, prevTotals: Set[Int] = Set[Int]()): Int = {
+    var pastTotals = prevTotals
+    var firstDuplicate: Option[Int] = None
+    var lastTotal = 0
+    forEachTotalFromFile(fileName, (newTotal: Int) => {
+      lastTotal = newTotal + startingTotal
+      if (pastTotals contains lastTotal) {
+        firstDuplicate = Some(lastTotal)
+        false
+      } else {
+        pastTotals = pastTotals + lastTotal
+        true
+      }
+    })
+    firstDuplicate match {
+      case Some(answer) => answer
+      case None => calculateFirstRepeatedTotalFromFile(fileName, lastTotal, pastTotals)
     }
   }
 }
