3929f68765b14b7949aa46ab47a187506e3b961d
ccolin84 <ccolin84@gmail.com>
2018-12-16 12:56:12 -0500

    solve part 2

--- day-2/src/main/scala/Day2.scala
+++ day-2/src/main/scala/Day2.scala
@@ -5,6 +5,8 @@ object Day2 {
     val inputFile = "src/input.txt"
     val checksum = getChecksum(inputFile)
     println(s"The checksum is $checksum")
+    val matchingLetters = getMatchingLettersForCorrectBoxes(inputFile)
+    println(s"The matching letters are: $matchingLetters")
   }
 
   def foreachInputFileLine(inputFile: String, handler: String => Boolean) = {
@@ -22,7 +24,7 @@ object Day2 {
     }
   }
 
-  def getMetricsFromLine(line: String): Tuple2[Boolean, Boolean] = {
+  def getMetricsFromBoxId(line: String): Tuple3[Boolean, Boolean, String] = {
     var charCounts = Map[Char, Int]()
     var twoOfAChar = Set[Char]()
     var threeOfAChar = Set[Char]()
@@ -38,18 +40,62 @@ object Day2 {
           twoOfAChar -= char
         }
       })
-    (twoOfAChar.nonEmpty, threeOfAChar.nonEmpty)
+    (twoOfAChar.nonEmpty, threeOfAChar.nonEmpty, line)
+  }
+
+  def getValidBoxIds(inputFile: String): Tuple2[Set[String], Set[String]] = {
+    var twoMatchingCharsBoxIds = Set[String]()
+    var threeMatchingCharsBoxIds = Set[String]()
+    foreachInputFileLine(inputFile, line => {
+      var (twoCount, threeCount, boxId) = getMetricsFromBoxId(line)
+      if (twoCount) twoMatchingCharsBoxIds += boxId
+      if (threeCount) threeMatchingCharsBoxIds += boxId
+      true
+    })
+    return (twoMatchingCharsBoxIds, threeMatchingCharsBoxIds)
   }
 
   def getChecksum(inputFile: String): Int = {
-    var totalTwoCounts = 0
-    var totalThreeCounts = 0
-    foreachInputFileLine(inputFile, line => {
-      var (twoCount, threeCount) = getMetricsFromLine(line)
-      if (twoCount) totalTwoCounts += 1
-      if (threeCount) totalThreeCounts += 1
-      true
-    })
-    return totalTwoCounts * totalThreeCounts
+    var (twoMatchingChars, threeMatchingChars) = getValidBoxIds(inputFile)
+    return twoMatchingChars.size * threeMatchingChars.size
+  }
+
+  def getStrWithoutPositionalMismatchingChars(s1: String, s2: String) = {
+    s1
+      .zip(s2)
+      .foldRight("")((charPair, acc) =>
+        charPair match {
+          case (c1, c2) => if (c1 == c2) c1 + acc else acc
+        }
+      )
+  }
+
+  def getMatchingLettersForCorrectBoxes(inputFile: String): String = {
+    var (twoMatchingChars, threeMatchingChars) = getValidBoxIds(inputFile)
+    var validBoxes = twoMatchingChars | threeMatchingChars
+    var matchingChars: Option[String] = None
+    var validBoxIds = validBoxes.toList
+
+    validBoxIds
+      .zipWithIndex
+      .withFilter(? => matchingChars == None)
+      .foreach({
+        case (boxId, inx) => {
+          val remainingBoxes = validBoxIds.drop(inx + 1)
+          remainingBoxes
+            .withFilter(? => matchingChars == None)
+            .foreach(innerBoxId => {
+              val strWithoutMismatchingChars = getStrWithoutPositionalMismatchingChars(boxId, innerBoxId)
+              if (strWithoutMismatchingChars.size == (boxId.size - 1)) {
+                matchingChars = Some(strWithoutMismatchingChars)
+              }
+            })
+        }
+      })
+
+    matchingChars match {
+      case Some(answer) => answer
+      case None => "No matching boxes found!"
+    }
   }
 }
